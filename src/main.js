d223d262215d83189795d90ee455bbab036880cc
luisaromero <luisa.marcela.romero@gmail.com>
2019-05-31 01:53:37 -0400

    agregando pantalla de registro de usuario

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+//Dando paso a la pantalla de ingreso de usuario
 document.getElementById('inicia').addEventListener('click',()=>{
 document.getElementById('registro').style.display='none'
 document.getElementById('principal').style.display='none'
+})
+//Dando paso a la pantalla de registro de usuario
+document.getElementById("registrate").addEventListener("click",()=>{
+    document.getElementById('principal').style.display='none'
+    document.getElementById('ingreso').style.display='none'
 })
