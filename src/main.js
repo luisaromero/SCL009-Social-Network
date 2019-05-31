39e024e370c543452d10b866257309504563605d
luisaromero <luisa.marcela.romero@gmail.com>
2019-05-31 01:44:59 -0400

    primera historia de usuario, agregando estilos al css y primera y segunda pantalla

---
+++ src/main.js
@@ -0,0 +1,4 @@
+document.getElementById('inicia').addEventListener('click',()=>{
+document.getElementById('registro').style.display='none'
+document.getElementById('principal').style.display='none'
+})
