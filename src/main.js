443121472d0b61d2678cf34431a355fabf01ac96
luisaromero <luisa.marcela.romero@gmail.com>
2019-06-01 00:38:14 -0400

    agregando estilos a las pantallas provisorios

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ document.getElementById('inicia').addEventListener('click',()=>{
 document.getElementById('registro').style.display='none'
 document.getElementById('principal').style.display='none'
 document.getElementById('ingreso').style.display='block'
-
+document.getElementById('cerrar').style.display='none'
 })
 //Dando paso a la pantalla de registro de usuario
 document.getElementById("registrate").addEventListener("click",()=>{
     document.getElementById('principal').style.display='none'
     document.getElementById('ingreso').style.display='none'
     document.getElementById('registro').style.display='block'
+    document.getElementById('cerrar').style.display='none'
 })
 
