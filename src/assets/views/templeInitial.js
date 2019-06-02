f8f4cad77c48424b40d84f49f7b84b0bd57d235d
luisaromero <luisa.marcela.romero@gmail.com>
2019-06-01 22:57:20 -0400

    detalles al css

--- src/assets/views/templeInitial.js
+++ src/assets/views/templeInitial.js
@@ -5,8 +5,7 @@ export const templateHome = () => {
     document.getElementById('root').innerHTML = ` <button id="inicia">Inicia sesión</button>
     <h2>O</h2>
     <button id="registrate">Registrate</button>
-    <button id="google">Accede con google</button>
-    </div>`
+    <button id="google">Accede con google</button>`
   document.getElementById('registrate').addEventListener('click', () => {
     templateAbout();
     window.location.hash = '#/about';
