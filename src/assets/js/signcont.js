837ac0b7626afc57e17dd2a2e9e2e43ea8913a48
luisaromero <luisa.marcela.romero@gmail.com>
2019-06-03 14:23:23 -0400

    agregando funcion observador

--- src/assets/js/signcont.js
+++ src/assets/js/signcont.js
@@ -1,39 +1,57 @@
 //observador que ve si entra o se registra un usuarie antigue o nuevx
-import {btnIngresar, btnEnviar, btnCerrar} from './sign.js'
 
-btnIngresar();
-btnEnviar();
-btnCerrar();
 
 export function observador(){
   
-    console.log("existe usuario activo")
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-              aparece();//llamamos a la función que nos muestra lo que puede ver usuarix logeadx
-          // User is signed in.
-          var displayName = user.displayName;
-          var email = user.email;
-          console.log(email)
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var isAnonymous = user.isAnonymous;
-          var uid = user.uid;
-          var providerData = user.providerData;
-          // ...
-        } else {
-            console.log("no existe usuario activo")
-          // User is signed out.
-          // ...
-        }
-      });email-password.html
+  console.log("existe usuario activo")
+  firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+            aparece();//llamamos a la función que nos muestra lo que puede ver usuarix logeadx
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        console.log(user.emailVerified)
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        // ...
+      } else {
+          console.log("no existe usuario activo")
+        // User is signed out.
+        // ...
+      }
+    });email-password.html
 }
 observador();
 
+
+
 export function aparece(){// llamamos al div dodne se crea lo que puede ver el o la usuarix activx esta funcion se llama en la funcion de observador
-    let contenido = document.getElementById("contenido");
-    contenido.innerHTML= `
-   <h5> "solo puede ver esto unx usuarix activx"</h5>
-    <button onclick="cerrar()">cerrar sesión</button>
-    `
+  let contenido = document.getElementById("contenido");
+  contenido.innerHTML= `
+ <h5> "solo puede ver esto unx usuarix activx"</h5>
+
+ <button id="cerrar" type="button">cierra sesión</button>
+  `
 }
+aparece();
+//cerrar sesion
+/*export const btnCerrar=document.getElementById("cerrar").addEventListener ("click",()=>{
+
+  console.log("esta cerrando")
+
+  firebase.auth().signOut()
+  .then(function(){
+      console.log("saliirrrr")
+  })
+  .catch(function(error){
+      console.log(error)
+  })
+ 
+
+
+  
+})
+btnCerrar();*/
