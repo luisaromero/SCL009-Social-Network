32f02222932808eb3d2e7b0efd9dfd80e557bb63
lau <laura.javiera77@gmail.com>
2019-06-03 17:21:39 -0400

    con funciones

--- src/assets/views/templeInitial.js
+++ src/assets/views/templeInitial.js
@@ -2,12 +2,18 @@
 import { templateAbout } from './templeSigin.js';
 
 export const templateHome = () => {
-    document.getElementById('root').innerHTML = ` <button id="inicia">Inicia sesión</button>
+    document.getElementById('root').innerHTML = ` 
+    <div class=contenedor>
+    <button id="inicia" class="btn">Inicia sesión</button>
     <h2>O</h2>
-    <button id="registrate">Registrate</button>
-    <button id="google">Accede con google</button>`
+    <button id="registrate" class="btn">Registrate</button>
+    <button id="google">Accede con google</button>
+    </div>`
+    
   document.getElementById('registrate').addEventListener('click', () => {
+   
     templateAbout();
+ 
     window.location.hash = '#/registrate';
   })
 }
