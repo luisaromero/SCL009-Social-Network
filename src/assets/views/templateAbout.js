7c2ea602040cc65fea386ff81aaec8b19d03c166
luisaromero <luisa.marcela.romero@gmail.com>
2019-06-05 17:16:40 -0400

    estilos al css

--- src/assets/views/templateAbout.js
+++ src/assets/views/templateAbout.js
@@ -2,9 +2,16 @@ import { templateStart } from './templateStart.js'
 
 
 export const templateAbout =() => {
+  document.getElementById( "conteinerHead").innerHTML= ` 
+  <div class= "conteinerH">
+  <h1>KETRAWE</h1>
+  </div>
+  `
     document.getElementById('root').innerHTML = `
+    <div class="contenedor">
   <p>Quienes somos ... </p>
-  <button id="back" class="btn">Atras</button>
+  <button id="atras" ></button>
+  </div>
     `
     document.getElementById('back').addEventListener('click', () => {
         templateStart();
--- src/assets/views/templateRegister.js
+++ src/assets/views/templateRegister.js
@@ -6,27 +6,43 @@ import {validateRegister} from '../js/auth.js'
 verificar();
 
 export const templateRegister = () => {
-  document.getElementById('root').innerHTML = `<h4>Registrate</h4>
+  document.getElementById( "conteinerHead").innerHTML= ` 
+  <div class= "conteinerH">
+  <h1>KETRAWE</h1>
+  </div>
+  `
+  document.getElementById('root').innerHTML = `
+ 
+  <div class="contenedor">
+  <h4>Registrate</h4>
   <input id="mail" type="text" placeholder="Ingresa tu correo">
   <p id ="invalidemail"><p>
   <input id= "contraseña" type="password" placeholder="Ingresa tu contraseña">
   <p id ="invalidpassword"><p>
-  <button id="enviar" type="button">Enviar</button>
+  <button id="enviar" class="btn1" >Enviar</button>
   <button id="atras" type="button"></button>
+  </div>
     `
     document.getElementById('enviar').addEventListener('click', () => {
       let email= document.getElementById("mail").value;
        let  password=document.getElementById("contraseña").value;
-       if(email ===""){
-        document.getElementById('invalidemail').innerHTML=`Debes ingresar un email`;
-    }
-    if (password ===""){
-        document.getElementById('invalidpassword').innerHTML=`Debes ingresar constraseña`;
-    }
-      
-    btnEnviar(email, password,name);
-    validateRegister(email , password) 
-    })
+       let verif=validateRegister(email , password);
+       if (verif=== false){
+         alert("pon un correo y contraseña valido")
+       }
+       if (password.length <=6){
+        document.getElementById("invalidpassword").innerHTML="Ingresa una contraseña mayor a 6 digitos"
+       } 
+    
+    
+    
+       if(verif=== true) { 
+           btnEnviar(email, password,name);
+   
+    document.getElementById('invalidemail').innerHTML="";
+    document.getElementById('invalidpassword').innerHTML="";
+     } })
+
   document.getElementById('atras').addEventListener('click', () => {
     // Nos lleva al inicio de la pagina
     // cambiar el hash a #/inicio
